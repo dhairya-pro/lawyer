1d3076f458bf2cf49a7d4fbe9ecb242cf4dc71a7
dhairya-pro <shahdhairya069@gmail.com>
2025-05-10 18:14:26 +0530

    about

--- frontened/src/components-about/About.jsx
+++ frontened/src/components-about/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LegalAdvisorCard from './LegalAdvisorCard'
 import TestimonialCarousel from './TestimonialCarousel'
+import HeroSection from '../components-home/HeroSection'
 const About = () => {
   return (
     <div>
         <div className="relative w-full bg-white overflow-hidden py-10 sm:py-8 md:py-12 px-4 md:px-8"></div> 
         <LegalAdvisorCard/>
         <TestimonialCarousel/>
+        <HeroSection/>
     </div>
   )
 }
